fd31ac0a9b118d843c348e5a6083f3d1bbdca09d
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-01-24 15:22:47 +0100

    Input

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,8 +1,62 @@
 #include "print.h"
 
+typedef int bool;
+#define true 1
+#define false 0
+
+#include <stdio.h>
+
+const char* input();
+void execute(const char* cmd);
+void output(const char* s, bool ln);
+void _puts(const char* s);
+void _putchar(char c);
+
+char* newline = "\n\r";
+char* version = "0.0.1 PRE-ALPHA";
 
 void kernel_main(){
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("Welcome To Blue OS with 64-bit kernel!");
+    output("Type 'help' for a list of commands.",1);
+    output("",1);
+    while(1==1) {
+        output("> ",0);
+        execute(input());
+        output("",1);
+    }
+}
+void execute(const char* cmd) {
+    output(cmd,1);
+}
+
+const char* input() {
+    char inpt[2] = {0,0};
+    scanf("%c",inpt);
+    return inpt;
+}
+
+void output(const char* s, bool ln) {
+    _puts(s);
+    if (ln==1) {
+        _puts(newline);
+    }
+}
+
+void _puts(const char* s) {
+    while(*s != 0) {
+        _putchar(*s);
+        s++;
+    }
+}
+
+void _putchar(char c) {
+    __asm {
+        mov ah, 0Eh
+        mov al, [c]
+        mov bh, 0
+        mov bl, 0Fh
+        int 10h
+    }
 }
