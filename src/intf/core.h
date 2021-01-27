49ebff1980a55e03bf79d6b6a7bc8e81079cb2d0
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-01-27 14:18:48 +0100

    Update core.h

--- src/intf/core.h
+++ src/intf/core.h
@@ -1,7 +1,25 @@
 #pragma once
+#ifndef HK_CORE
+#define HK_CORE
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <map>
+
+#include <fstream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <chrono>
+#include <sstream>
+#include <string>
+#include <math.h>
 
 #include <stdio.h>
-#include <fstream>
 #include <math.h>
 #include <stdint.h>
 #include <stddef.h>
+
+#endif
