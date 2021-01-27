540734fbff0c1c6e14cd2e2e881fe07d619cc537
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-01-27 14:17:46 +0100

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,13 +1,20 @@
 #include "print.h"
+#include "core.h"
 
 #include <stdio.h>
+
+char version = 0.01;
+char ram;
 
 //main functions:
 
 void kernel_main(){
     print_clear();
     print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
-    print_str("Welcome To Blue OS with 64-bit kernel!");
+    print_str("Welcome To Blue OS with 64-bit kernel! "+ version );
 }
+//Ram functions:
 
+void checkRAM(){
 
+}
