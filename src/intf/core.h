02426c7b81f967c3e4a5f4a3e9145713ba46efa0
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-01-30 17:44:46 +0100

    Update core.h

--- src/intf/core.h
+++ src/intf/core.h
@@ -1,21 +1,6 @@
 #pragma once
 #ifndef BL_CORE
 #define BL_CORE
-#include <vector>
-#include <array>
-#include <unordered_map>
-#include <map>
-
-#include <fstream>
-#include <memory>
-#include <utility>
-#include <algorithm>
-#include <functional>
-
-#include <chrono>
-#include <sstream>
-#include <string>
-#include <math.h>
 
 #include <stdio.h>
 #include <math.h>
