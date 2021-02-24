7203a8ea95b36b45e3c6116297e1789e5d246f16
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-02-24 16:19:12 +0100

    Update main.c
    Co-Authored-By: lubek <70327203+lubek-dc@users.noreply.github.com>

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -7,4 +7,4 @@ void kernel_main(){
     print_clear();
     print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
     print_str("Welcome To Blue OS with 64-bit kernel! ");
-}
+}//Soon more functions!
